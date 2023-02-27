875820584cc78b69d2115c32a2be9529c420de1c
elolengodfrey <elolengodfrey@gmail.com>
2023-02-27 05:40:42 +0100

    0. FizzBuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,4 +1,4 @@
-	#!/usr/bin/python3
+#!/usr/bin/python3
 """ FizzBuzz
 """
 import sys
@@ -14,7 +14,7 @@ def fizzbuzz(n):
     if n < 1:
         return
 
-      tmp_result = []
+    tmp_result = []
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
@@ -27,7 +27,6 @@ def fizzbuzz(n):
     print(" ".join(tmp_result))
 
 
-
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print("Missing number")
@@ -37,4 +36,3 @@ if __name__ == '__main__':
 
     number = int(sys.argv[1])
     fizzbuzz(number)
-    
