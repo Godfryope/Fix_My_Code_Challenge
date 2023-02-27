584f760910e6a6d308fa414ea07dd9a6799994d1
elolengodfrey <elolengodfrey@gmail.com>
2023-02-27 05:40:01 +0100

    0. FizzBuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -14,7 +14,7 @@ def fizzbuzz(n):
     if n < 1:
         return
 
-    tmp_result = []
+      tmp_result = []
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
@@ -25,6 +25,7 @@ def fizzbuzz(n):
         else:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
+
 
 
 if __name__ == '__main__':
